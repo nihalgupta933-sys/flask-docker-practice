d621780823c85dd75df1f99b17b37cdb3b73ab2e
Nihal Gupta <nihalgupta933@gmail.com>
2026-08-13 13:58:41 +0530

    Delete flask-docker-practice directory

--- flask-docker-practice/app.py
+++
@@ -1,9 +0,0 @@
-from flask import  Flask
-import os
-app=Flask(__name__)
-@app.route('/',methods=['GET'])
-def home():
-    return "HELLO MY NAME IS NIHAL GUPTA , PRACTICING DOCKER"
-if __name__=='__main__':
-    app.run(debug=True,host="0.0.0.0",port=5000)
-
